6d0380cd4fd6cfc5552b6f2e7d6efd0f08327c4b
Otis Virginie <otissv@gmail.com>
2021-02-01 17:05:32 +0100

    netlify form

--- src/features/Subscribe/Subscribe.tsx
+++ src/features/Subscribe/Subscribe.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Textbox } from "@redesign-system/react-ui/Textbox";
 
 import { SubscribeProps } from "./types.subscribe";
 import { CTAButton } from "../../components/Button";
 import { Form } from "../../components/Form";
 import { Section } from "../../components/Section";
+import { Typography } from "@redesign-system/react-ui/Typography";
 
 export function Subscribe({
   buttonLabel,
@@ -17,10 +18,59 @@ export function Subscribe({
   title,
 }: SubscribeProps): JSX.Element {
   const classNames = `Subscribe ${className}`;
+  const [email, setEmail] = useState("");
+  const [hasError, setHasError] = useState(false);
+  const [showThankYou, setShowThankYou] = useState(false);
 
   const css = `
     background: var(--background-color-3);
   `;
+
+  function validateEmail(value: string): boolean {
+    return value.length < 5 || !value.match(/@\w/);
+  }
+
+  function handleOnBlur(e: any) {
+    const value = e.target.value;
+
+    const isValid = validateEmail(value);
+
+    // eslint-disable-next-line functional/no-conditional-statement
+    if (isValid !== hasError) {
+      // eslint-disable-next-line functional/no-expression-statement
+      setHasError(isValid);
+    }
+  }
+
+  function handleOnChange(e: any) {
+    const value = e.target.value;
+    // eslint-disable-next-line functional/no-expression-statement
+    setEmail(value);
+  }
+
+  function handleOnSubmit(e: any) {
+    // eslint-disable-next-line functional/no-expression-statement
+    e.preventDefault();
+
+    const isValid = validateEmail(email);
+
+    // eslint-disable-next-line functional/no-conditional-statement
+    if (isValid === true) {
+      // eslint-disable-next-line functional/no-expression-statement
+      isValid !== hasError && setHasError(isValid);
+      // eslint-disable-next-line functional/no-conditional-statement
+    } else {
+      // eslint-disable-next-line functional/no-expression-statement
+      fetch("/", {
+        method: "POST",
+        body: new URLSearchParams(email).toString(),
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      })
+        // .then((response: Response) => response.json())
+        .then(() => setShowThankYou(true))
+        .catch(console.log);
+    }
+  }
   return (
     <Section
       id="subscribe"
@@ -50,6 +100,9 @@ export function Subscribe({
         </p>
 
         <Form
+          name="subscribe"
+          method="POST"
+          data-netlify="true"
           d="block"
           w="100%"
           mq={{
@@ -63,14 +116,20 @@ export function Subscribe({
         >
           <Textbox
             id={inputId}
-            appearance="SUBSCRIBE"
+            value={email}
             label={inputLabel}
             placeholder={inputPlaceholder}
+            onBlur={handleOnBlur}
+            onChange={handleOnChange}
+            required={true}
+            type="email"
+            appearance="SUBSCRIBE"
             fs="20px"
             h="52px"
             flex={1}
             mb={7}
             mx={0}
+            border={hasError}
             css={`
               border-bottom-left-radius: 30px;
               border-top-left-radius: 30px;
@@ -90,6 +149,8 @@ export function Subscribe({
           />
 
           <CTAButton
+            type="submit"
+            onClick={handleOnSubmit}
             mq={{
               md: {
                 min: {
@@ -102,6 +163,15 @@ export function Subscribe({
             {buttonLabel}
           </CTAButton>
         </Form>
+        <Typography
+          color="#F29B9B"
+          mt={3}
+          visibility={hasError ? "visible" : "hidden"}
+        >
+          Invalid Email address.
+        </Typography>
+
+        {showThankYou && <p>Thank you for subscribing.</p>}
       </div>
     </Section>
   );
